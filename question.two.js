7234a4bf327bc4d07f51ea7a521aca6f4b8b44f4
Johnny M Ferreira <johnnymaikeo@gmail.com>
2017-07-25 23:22:33 -0300

    including answer to question number 2

--- question.two.js
+++ question.two.js
@@ -4,6 +4,21 @@
  * Array.sort() method. Try to build a function that would perform fast.  
  * Also, explain why using Array.sort() isn't a good idea when sorting  
  * large arrays.  
+ * 
+ * Response: While there isn't a definition to which sorting algorithm a 
+ * browser should implement on array.sort() we can't ensure it will be the 
+ * fastest or best option for a given data set. A benchmark using both 
+ * Google Chrome and Safari demonstrated the performance of a simple quick 
+ * sort implementation over both browsers implementation to array.sort() 
+ * when sorting an array of 50.000.000 elements.
+ * 
+ * Google Chrome:
+ * quicksort(): 33655.604736328125ms
+ * array.sort(): 59112.30908203125ms
+ * 
+ * Safari:
+ * quicksort(): 35472.037ms
+ * array.sort(): passed 5min
  */
 
 function quicksort(array, left, right) {
